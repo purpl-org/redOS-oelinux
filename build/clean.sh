9debc4c2ad9bf30e647d976ffcad73c84a709638
Wire <kerigancreighton@gmail.com>
2025-05-14 15:48:55 -0500

    use correct docker container

--- build/clean.sh
+++ build/clean.sh
@@ -13,10 +13,10 @@ fi
 
 DIRPATH="$(pwd)"
 
-docker build --build-arg DIR_PATH="${DIRPATH}" --build-arg USER_NAME=$(whoami) --build-arg UID=$(id -u $USER) --build-arg GID=$(id -g $USER) -t vic-yocto-builder-2 build/
+#docker build --build-arg DIR_PATH="${DIRPATH}" --build-arg USER_NAME=$(whoami) --build-arg UID=$(id -u $USER) --build-arg GID=$(id -g $USER) -t vic-yocto-builder-2 build/
 
 docker run -it \
     -v $(pwd)/anki-deps:${HOME}/.anki \
     -v $(pwd):$(pwd) \
     -v $(pwd)/build/cache:${HOME}/.ccache \
-    vic-yocto-builder-2 bash -c "cd $(pwd)/poky && source build/conf/set_bb_env.sh && MACHINE=apq8009-robot VARIANT=perf DISTRO=msm-perf PRODUCT=robot bitbake -c cleanall $@"
+    vic-yocto-builder-3 bash -c "cd $(pwd)/poky && source build/conf/set_bb_env.sh && MACHINE=apq8009-robot VARIANT=perf DISTRO=msm-perf PRODUCT=robot bitbake -c cleanall $@"
