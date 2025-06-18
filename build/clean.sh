7948b9af70d7852f57fc52d230be6815a03bb77a
OpenEmbedded <oe.patch@oe>
2025-06-17 20:48:03 -0500

    fix connman version

--- build/clean.sh
+++ build/clean.sh
@@ -15,8 +15,14 @@ DIRPATH="$(pwd)"
 
 #docker build --build-arg DIR_PATH="${DIRPATH}" --build-arg USER_NAME=$(whoami) --build-arg UID=$(id -u $USER) --build-arg GID=$(id -g $USER) -t vic-yocto-builder-2 build/
 
+VARI="perf"
+
+if [[ ${PROD} == "1" ]]; then
+	VARI="user"
+fi
+
 docker run -it --rm \
     -v $(pwd)/anki-deps:${HOME}/.anki \
     -v $(pwd):$(pwd) \
     -v $(pwd)/build/cache:${HOME}/.ccache \
-    vic-yocto-builder-5 bash -c "cd $(pwd)/poky && source build/conf/set_bb_env.sh && MACHINE=apq8009-robot VARIANT=perf DISTRO=msm-perf PRODUCT=robot bitbake -c cleanall $@"
+    vic-yocto-builder-5 bash -c "cd $(pwd)/poky && source build/conf/set_bb_env.sh && MACHINE=apq8009-robot VARIANT=perf DISTRO=msm-${VARI} PRODUCT=robot bitbake -c cleanall $@"
