319761e0e17b83bcdbbeb0d6df51ac026758db91
froggitti <milkysoibeans@gmail.com>
2026-01-15 20:26:45 -0500

    Update everything

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 git pull origin main
-cd poky/poky
-git pull origin master
-cd ../../
-cd poky/meta-openembedded
-git pull origin master
-cd ../../
+#cd poky/poky
+#git pull origin master
+#cd ../../
+#cd poky/meta-openembedded
+#git pull origin master
+#cd ../../
 cd anki/wired
 git pull origin main --recurse-submodules
 cd ../../
